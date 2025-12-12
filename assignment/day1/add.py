2519e87b08be7801cfd782ddde8bb7e48abada45
Snehit <snehitp5555@gmail.com>
2025-12-12 23:09:18 +0530

    que 1

---
+++ assignment/day1/add.py
@@ -0,0 +1,4 @@
+num1=int(input("enter a number"))
+num2=int(input("enter a number2"))
+num3=num1+num2
+print("addition",+num3)
---
+++ assignment/day1/swapp.py
@@ -0,0 +1,7 @@
+num1=int(input("enter the  1st no."))
+num2=int(input("enter the  2nd no."))
+temp=num1
+temp=num2
+temp=temp
+print("enter number 1:",+num2)
+print("enter number 2:",+num1)
